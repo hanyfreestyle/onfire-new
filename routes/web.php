01b8185e1bbb256426d3d2d40c33d98e7bba4d4f
Hany Darwish <hany.freestyle4u@gmail.com>
2023-10-07 22:40:11 +0300

    Clean view 2

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,6 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 //Auth::viaRemember();
 
 
-
-//Route::group(['prefix' => LaravelLocalization::setLocale()], function(){
-//    Route::get('/', [WebPageController::class, 'HomePage'])->name('Page_HomePage');
-//});
-
-
 Route::group(['prefix' => LaravelLocalization::setLocale()], function(){
     Route::group(['prefix'=>'admin'],function(){
         Route::get('/login', [AuthAdminController::class, 'Adminlogin'])->name('login');
@@ -62,59 +56,11 @@ Route::get('/CartEmpty',
 Route::get(LaravelLocalization::transRoute('routes.ContactUs'),
     [WebPageController::class, 'ContactUs'])->name('Page_ContactUs');
 
-/*
-Route::group(['prefix' => LaravelLocalization::setLocale(),
-    'middleware' => [ 'localize' ]], function () {
+Route::post('ContactSend',
+    [WebPageController::class, 'ContactSend'])->name('Page_ContactSend');
 
-    Route::get(LaravelLocalization::transRoute('routes.AboutUs'),
-        [WebPageController::class, 'AboutUs'])->name('Page_AboutUs');
+Route::get('/thanks',
+    [WebPageController::class, 'ContactUsThanks'])->name('Page_ContactUsThanks');
 
-    Route::get(LaravelLocalization::transRoute('routes.OurClient'),
-        [WebPageController::class, 'OurClient'])->name('Page_OurClient');
-
-    Route::get(LaravelLocalization::transRoute('routes.Careers'),
-        [WebPageController::class, 'Careers'])->name('Page_Careers');
-
-
-    Route::get(LaravelLocalization::transRoute('routes.ContactUs'),
-        [WebPageController::class, 'ContactUs'])->name('Page_ContactUs');
-
-    Route::post('ContactSend',
-        [WebPageController::class, 'ContactSend'])->name('Page_ContactSend');
-
-    Route::get('/thanks',
-        [WebPageController::class, 'ContactUsThanks'])->name('Page_ContactUsThanks');
-
-
-    Route::get(LaravelLocalization::transRoute('routes.TermsConditions'),
-        [WebPageController::class, 'TermsConditions'])->name('Page_TermsConditions');
-
-
-    Route::get(LaravelLocalization::transRoute('routes.FaqList'),
-        [WebPageController::class, 'FaqList'])->name('Page_FaqList');
-
-    Route::get(LaravelLocalization::transRoute('routes.FaqCatView'),
-        [WebPageController::class, 'FaqCatView'])->name('Page_FaqCatView');
-
-
-    Route::get(LaravelLocalization::transRoute('routes.LatestNews'),
-        [WebPageController::class, 'LatestNews'])->name('Page_LatestNews');
-
-    Route::get(LaravelLocalization::transRoute('routes.LatestNews_View'),
-        [WebPageController::class, 'LatestNews_View'])->name('LatestNews_View');
-
-
-    Route::get(LaravelLocalization::transRoute('routes.MainCategory'),
-        [WebPageController::class, 'MainCategory'])->name('Page_MainCategory');
-
-    Route::get(LaravelLocalization::transRoute('routes.WebCategoryView'),
-        [WebPageController::class, 'WebCategoryView'])->name('Page_WebCategoryView');
-
-    Route::get(LaravelLocalization::transRoute('routes.WebProductView'),
-        [WebPageController::class, 'WebProductView'])->name('Page_WebProductView');
-
-
-
-
-});
-*/
+Route::get(LaravelLocalization::transRoute('routes.TermsConditions'),
+    [WebPageController::class, 'TermsConditions'])->name('Page_TermsConditions');
