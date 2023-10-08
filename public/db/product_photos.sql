0664aaab660e315f03daa727d432afc0a2d76972
Hany Darwish <hany.freestyle4u@gmail.com>
2023-10-08 16:13:02 +0300

    add pro photos

--- public/db/product_photos.sql
+++ public/db/product_photos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 08, 2023 at 12:22 PM
+-- Generation Time: Oct 08, 2023 at 03:10 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
